ac7934a2b1e14e10ed97a71f7ba8c64b23e93242
Federico Vazzoler <vazzoler.federico@gmail.com>
2023-10-12 11:53:02 +0200

    Fix bug in angle names

--- wremnants/include/csVariables.h
+++ wremnants/include/csVariables.h
@@ -42,35 +42,6 @@ struct CSVars {
 
 };
 
-CSVars CalccsSineCosThetaPhiVanilla(const PtEtaPhiMVector& antilepton, const PtEtaPhiMVector& lepton) {
-    PxPyPzEVector antilepton_v(antilepton);
-    PxPyPzEVector dilepton = antilepton_v + PxPyPzEVector(lepton);
-    const int zsign = std::copysign(1.0, dilepton.z());
-    const double energy = 6500.;
-    PxPyPzEVector proton1(0., 0., zsign*energy, energy);
-    PxPyPzEVector proton2(0., 0., -1.*zsign*energy, energy);
-
-    auto dilepCM = dilepton.BoostToCM();
-    ROOT::Math::Boost dilepCMBoost(dilepCM);
-
-    auto pro1boost = unitBoostedVector(dilepCMBoost, proton1);
-    auto pro2boost = unitBoostedVector(dilepCMBoost, proton2);
-    auto antilepton_boost = unitBoostedVector(dilepCMBoost, antilepton_v);
-    auto csFrame = (pro1boost-pro2boost).Unit();
-    auto csYaxis = cross(pro1boost, pro2boost).Unit();
-    auto csXaxis = cross(csYaxis, csFrame).Unit();
-
-    double costheta = dot(csFrame, antilepton_boost);
-    auto csCross = cross(csFrame, antilepton_boost);
-    double sintheta = csCross.R()/(csFrame.R()*antilepton_boost.R());
-
-    double sinphi = dot(csYaxis, antilepton_boost)/sintheta;
-    double cosphi = dot(csXaxis, antilepton_boost)/sintheta;
-
-    CSVars angles = {sintheta, costheta, sinphi, cosphi};
-    return angles;
-}
-
 CSVars CalccsSineCosThetaPhi(const PtEtaPhiMVector& antilepton, const PtEtaPhiMVector& lepton) {
 	// following https://arxiv.org/pdf/2006.11382.pdf pag. 18 eq. 2.56 and 2.57
   	PxPyPzEVector p1(lepton);
@@ -83,19 +54,19 @@ CSVars CalccsSineCosThetaPhi(const PtEtaPhiMVector& antilepton, const PtEtaPhiMV
   	PxPyPzEVector pa(0., 0., zsign*energy, energy);
   	PxPyPzEVector pb(0., 0., -1.*zsign*energy, energy);
 
-	double cosTheta = 1. / (Q.M() * Q.Mt()) * ((p1.E() + p1.z()) * (p2.E() - p2.z()) - (p1.E() - p1.z()) * (p2.E() + p2.z()));
-	double sinTheta = std::sqrt(1 - cosTheta * cosTheta);
-	double cosPhi = 1. / sinTheta * (p1.Pt() * p1.Pt() - p2.Pt() * p2.Pt()) / (Q.Pt() * Q.Mt());
-	double sinPhi = 2. / sinTheta * (p1.y() * p2.x() - p2.y() * p1.x()) / (Q.Pt() * Q.M());
+	double costheta = 1. / (Q.M() * Q.Mt()) * ((p1.E() + p1.z()) * (p2.E() - p2.z()) - (p1.E() - p1.z()) * (p2.E() + p2.z()));
+	double sintheta = std::sqrt(1 - costheta * costheta);
+	double cosphi = 1. / sintheta * (p1.Pt() * p1.Pt() - p2.Pt() * p2.Pt()) / (Q.Pt() * Q.Mt());
+	double sinphi = 2. / sintheta * (p1.y() * p2.x() - p2.y() * p1.x()) / (Q.Pt() * Q.M());
 
 #if 1
-	cosTheta = cosTheta * zsign;
-	double Phi = std::atan2(sinPhi, cosPhi) * zsign;
-	cosPhi = std::cos(Phi);
-	sinPhi = std::sin(Phi);
+	costheta = costheta * zsign;
+	double Phi = std::atan2(sinphi, cosphi) * zsign;
+	cosphi = std::cos(Phi);
+	sinphi = std::sin(Phi);
 #endif
 
-	CSVars angles = {sinTheta, cosTheta, sinPhi, cosPhi};
+	CSVars angles = {sintheta, costheta, sinphi, cosphi};
     return angles;
 }
 
